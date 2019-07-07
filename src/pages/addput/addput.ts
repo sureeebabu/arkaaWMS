40456e5e2129bd893de541fb35c178fb8968d0f3
daijichennai <daijiworldtech@gmail.com>
2019-07-07 14:53:17 +0530

    Put Details

--- src/pages/addput/addput.ts
+++ src/pages/addput/addput.ts
@@ -11,6 +11,11 @@ import { Observable } from 'rxjs/Observable';
 })
 export class AddputPage {
   public intPutDetailsID;
+  public putDetailsJson:any =[];
+  public hfScanBoxQR:string;
+  public hfScanRackQR:string;
+  scanBoxQR:string;
+  scanRackQR:string;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -25,6 +30,35 @@ export class AddputPage {
 }
 
   ionViewDidLoad() {
+    this.getPutDetailsyID(this.intPutDetailsID);
+  }
+
+  getPutDetailsyID(putDetailsID) {
+    let data: Observable<any>;
+    // alert(custCode);
+    let url = this.myFunc.domainURL + "handlers/putMaster.ashx?mode=selPutDetByID&putDetailsID=" + putDetailsID;
+    let loader = this.loadingCtrl.create({
+      content: 'Fetching Data From Server...'
+    });
+    data = this.http.get(url);
+    loader.present().then(() => {
+      data.subscribe(result => {
+       // console.log(result);
+        this.putDetailsJson = result;
+        this.hfScanBoxQR = result[0].itemName;
+        this.hfScanRackQR = result[0].suggestedLocation;
+
+        console.log(this.putDetailsJson);
+        // alert(this.hfScanBoxQR);
+        // alert(this.hfScanRackQR);
+
+        loader.dismiss();
+      }, error => {
+        loader.dismiss();
+        console.log(error);
+        //alert('Error in List Claim');
+      });
+    });
   }
 
 }
