709d7c53854651212b52f5d3aac9e22cae6f3189
daijichennai <daijiworldtech@gmail.com>
2019-07-07 15:57:34 +0530

    Insert put details to stock

--- src/pages/addput/addput.ts
+++ src/pages/addput/addput.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { CommfuncProvider } from '../../providers/commfunc/commfunc';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
@@ -16,12 +16,18 @@ export class AddputPage {
   public hfScanRackQR:string;
   scanBoxQR:string;
   scanRackQR:string;
+  itemName:string;
+  itemCode:string;
+  itemQty:string;
+  putID:string;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private http: HttpClient,
     private loadingCtrl: LoadingController,
-    private myFunc: CommfuncProvider
+    private myFunc: CommfuncProvider,
+    public alertCtrl: AlertController,
 )
  {
     this.intPutDetailsID = this.navParams.get("putDetailsID");
@@ -45,10 +51,21 @@ export class AddputPage {
       data.subscribe(result => {
        // console.log(result);
         this.putDetailsJson = result;
-        this.hfScanBoxQR = result[0].itemName;
+        this.hfScanBoxQR = result[0].itemCode +"_" + result[0].itemName + "_" + result[0].itemQty;
         this.hfScanRackQR = result[0].suggestedLocation;
 
+        this.itemName = result[0].itemName;
+        this.itemCode = result[0].itemCode;
+        this.itemQty = result[0].itemQty;
+        this.putID= result[0].putID;
+
         console.log(this.putDetailsJson);
+
+        console.log("itemName = " + this.itemName);
+        console.log("itemCode = " + this.itemCode);
+        console.log("itemQty  = " + this.itemQty);
+        console.log("putID    = " + this.putID);
+
         // alert(this.hfScanBoxQR);
         // alert(this.hfScanRackQR);
 
@@ -60,5 +77,61 @@ export class AddputPage {
       });
     });
   }
+
+
+  submitFn(){
+    if(this.hfScanBoxQR === this.scanBoxQR && this.hfScanRackQR === this.scanRackQR){
+      alert("submit");
+      let data: Observable<any>;
+      let url = this.myFunc.domainURL + 'handlers/putMaster.ashx?mode=insPut&putID='+ this.putID + "&rackQR=" + this.scanRackQR + "&itemCode=" + this.scanBoxQR + "&itemName=" + this.itemName + "&itemQty=" + this.itemQty + "&userName=user1" + "&putDetailsID=" + this.intPutDetailsID;
+      // let queryParams = JSON.stringify({ 
+      //   putID: this.putID,
+      //   rackQR: this.scanRackQR,
+      //   itemCode: this.scanBoxQR,
+      //   itemName: this.itemName,
+      //   itemQty: this.itemQty,
+      //   userName: "user1",
+      //   putDetailsID: this.intPutDetailsID,
+      //  });
+  
+      let loader = this.loadingCtrl.create({
+        content: 'Inserting Data'
+      });
+  
+      data = this.http.post(url, "");
+      loader.present().then(() => {
+        data.subscribe(result => {
+          alert("success");
+          console.log(result);         
+          
+          loader.dismiss();
+        }, error => {
+          alert("failure");
+          console.log(error);
+          loader.dismiss();
+        });
+      });
+    }else{
+      this.alertMsgFn('Invalid Bar Code !...........');
+    }
+
+  }
+
+  alertMsgFn(msg){
+    let altsuccess = this.alertCtrl.create({
+      title: 'Success',
+      message: msg,
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            //this.navCtrl.push(CreditListPage);
+          }
+        }
+      ]
+    });
+    altsuccess.present();
+  }
+
 
 }
--- src/providers/commfunc/commfunc.ts
+++ src/providers/commfunc/commfunc.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CommfuncProvider {
 
-  public domainURL : string = "http://www.videomemes.in/";
-  //public domainURL : string = "http://localhost:49161/videoMemes/";
+  //public domainURL : string = "http://www.videomemes.in/";
+  public domainURL : string = "http://localhost:49161/videoMemes/";
   constructor(public http: HttpClient) {
     
   }
